fea4c81ec88190d22c241d4ba765836cc24c2af6
n-elmasry <almassrynada411@gmail.com>
2024-08-02 00:20:49 +0500

    task 4 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""sum_list"""
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> List[float]:
+    """returns their sum as a float."""
+    return sum(input_list)
