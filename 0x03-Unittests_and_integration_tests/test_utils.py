d4eab3e0c0e0d09320ae962e282d21334af84219
n-elmasry <almassrynada411@gmail.com>
2024-08-26 21:48:10 +0500

    task 1

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -22,10 +22,10 @@ class TestAccessNestedMap(unittest.TestCase):
         ({}, ("a",), KeyError),
         ({"a": 1}, ("a", "b"), KeyError),
     ])
-    def test_access_nested_map_exception(self, nested_map, path, expected_message):
+    def test_access_nested_map_exception(self, nested_map, path, exception):
         """test_access_nested_map_exception"""
 
-        with self.assertRaises(Exception):
+        with self.assertRaises(exception):
             access_nested_map(nested_map, path)
 
 
